c45ba3881b0b8918740b00e778015f7db75f8d5d
buenofonsecag <gustavo.fonseca@sptech.school>
2023-10-24 12:10:22 -0300

    validação de cpf e validação de campo em branco

--- site/web/Cadastro.js
+++ site/web/Cadastro.js
@@ -1,30 +1,42 @@
 
 function cadastrar() {
 
+    var representante = input_representante.value;
+    var nome_empresa = input_nome_empresa.value;
     var senha = input_senha.value;
     var email = input_email.value;
-    var senhainvalida = 0;
-    mensagem_invalida = "Sua senha é inválida insira pelo menos:";
- 
-    
-    // email possuiir @
-    if (email.indexOf("@") > -1) {
-        div_emailarroba.innerHTML = ""
+    var cnpj = input_cnpj.value;
+
+    if (representante == "" ||
+        nome_empresa == "" ||
+        senha == "" ||
+        email == "" ||
+        cnpj == "" ) {
+        alert("Preencha todos os campos")
+    }
+
+
+
+    // cnpj com 14 digitos
+    if (cnpj.length == 14) {
+        div_cnpj.innerHTML = ""
     } else {
-        div_emailarroba.innerHTML = "Seu email é inválido, insira um @"
+        div_cnpj.innerHTML = "Insira um CNPJ válido"
+    }
+
+    // email possuiir @
+    if (email.indexOf("@") == -1) {
+        div_emailarroba.innerHTML = "Seu email é inválido"
+    } else {
+        div_emailarroba.innerHTML = ''
     }
 
 
     // senha com 8 dígitos no mínimo
-    if (senha.length >= 8) {
-        div_8caracter.innerHTML = ''
-    } else {
-        senhainvalida += 1;
-        if (senhainvalida == 1) {
-            div_senha_invalida.innerHTML = `${mensagem_invalida}<br>`
-        }
+    if (senha.length < 8) {
         div_8caracter.innerHTML = '• Sua senha não possui pelo menos 8 caracteres !'
-        div_senha_invalida.innerHTML = ''
+    } else {
+        div_8caracter.innerHTML = ''
     }
 
     // senha possuir pelo menos um numero
@@ -38,45 +50,27 @@ function cadastrar() {
         senha.indexOf("7") > -1 ||
         senha.indexOf("8") > -1 ||
         senha.indexOf("9") > -1) {
-
         div_numero.innerHTML = ''
     } else {
-        senhainvalida += 1;
-        if (senhainvalida == 1) {
-            div_senha_invalida.innerHTML = `${mensagem_invalida}<br>`
-        }
-        div_numero.innerHTML = '• Não tem número'
-        div_senha_invalida.innerHTML = ''
-
+        div_numero.innerHTML = '• Não possui número'
     }
 
     // senha possuir pelo menos uma letra maiuscula
     if (senha.toLowerCase() == senha) {
-        senhainvalida += 1;
-        if (senhainvalida == 1) {
-            div_senha_invalida.innerHTML = `${mensagem_invalida}<br>`
-        }
-        div_maiuscula.innerHTML = '• Não tem letra maiúscula'
-        div_senha_invalida.innerHTML = ''
-
+        div_maiuscula.innerHTML = '• Não possui letra maiúscula'
     } else {
         div_maiuscula.innerHTML = ''
     }
 
     // senha possuir uma letra minuscula
     if (senha.toUpperCase() == senha) {
-        senhainvalida += 1;
-        if (senhainvalida == 1) {
-            div_senha_invalida.innerHTML = `${mensagem_invalida}<br>`
-        }
-        div_minuscula.innerHTML = '• Não tem letra minúscula'
-        div_senha_invalida.innerHTML = ''
-
+        div_minuscula.innerHTML = '• Não possui letra minúscula'
     } else {
         div_minuscula.innerHTML = ''
     }
 
     // senha possuir um caractere especial
+    // caracteres especias validados = !@#$ %^&* ()_ + {}[] |\\: ; \"'<>,.?/~-
     if (senha.indexOf("!") > -1 ||
         senha.indexOf("@") > -1 ||
         senha.indexOf("#") > -1 ||
@@ -99,7 +93,7 @@ function cadastrar() {
         senha.indexOf("?") > -1 ||
         senha.indexOf("'") > -1 ||
         senha.indexOf(`"`) > -1 ||
-        // senha.indexOf("\") > -1 ||
+        // senha.indexOf("\")> -1 ||
         senha.indexOf("<") > -1 ||
         senha.indexOf(">") > -1 ||
         senha.indexOf(",") > -1 ||
@@ -108,24 +102,9 @@ function cadastrar() {
         senha.indexOf("/") > -1 ||
         senha.indexOf("-") > -1 ||
         senha.indexOf("_") > -1) {
-        div_especial.innerHTML = ''
+        div_especial.innerHTML = ' '
     } else {
-        senhainvalida += 1;
-        if (senhainvalida == 1) {
-            div_senha_invalida.innerHTML = `${mensagem_invalida}<br>`
-        }
-        div_especial.innerHTML = '• Não tem caractere especial '
-        div_senha_invalida.innerHTML = ''
-
+        div_especial.innerHTML = '• Não possui caractere especial'
     }
-
-    senhainvalida=0; 
-
-    if (senhainvalida >=0) {
-        div_senha_invalida = "Sua senha é válida"
-        div_senha_invalida.innerHTML = ''
-
-    }
-
-    // caracteres especias validados = !@#$ %^&* ()_ + {}[] |\\: ; \"'<>,.?/~-
 }
+
