8abc78c66dfb8ecfd4b8932b3edafea3b84568d7
buenofonsecag <gustavo.fonseca@sptech.school>
2023-10-25 11:27:40 -0300

    mudanças nas validações de login

--- site/web/scripts/scripitLogin.js
+++ site/web/scripts/scripitLogin.js
@@ -3,11 +3,17 @@ function Fazerlogin() {
     var InputEmail = input_TelaLogin_Email.value;
     var InputSenha = input_TelaLogin_Senha.value;
 
-    if (InputEmail == 0 || InputEmail.indexOf("@") == -1) {
-        msgInputEmail.innerHTML = "Insira seu email!"; 
+    if (InputEmail == 0 
+        // || InputEmail.indexOf("@") == -1
+        ) {
+        msgInputEmail.innerHTML = "* Insira seu e-mail "; 
+    }else{
+        msgInputEmail.innerHTML = ""
     }
 
     if(InputSenha == 0 ){
-        msgInputSenha.innerHTML = "Insira a sua Senha!"; 
+        msgInputSenha.innerHTML = "* Insira sua senha"; 
+    }else{
+        msgInputSenha.innerHTML = ""
     }
 };
