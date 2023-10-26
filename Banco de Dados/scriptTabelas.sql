12e9247430726a69ef2f853d906ba7eaecabe9b6
pabloSantos49 <pablo.santos@sptech.school>
2023-10-26 13:44:52 -0300

    Alteran o campo dataHora na tabela registro

--- Banco de Dados/scriptTabelas.sql
+++ Banco de Dados/scriptTabelas.sql
@@ -1,3 +1,5 @@
+drop database strawTech;
+
 create database strawTech;
 use strawTech;
 
@@ -43,13 +45,18 @@ create table arduino (
 
 create table registro (
 	idRegistro int primary key auto_increment,
-	fkArduino int ,
+	fkArduino int,
 	foreign key (fkArduino) references arduino(idArduino),
-    lm35_temperatura float(5),
-    dht11_umidade float(5),
-    dataHora timestamp
+    lm35_temperatura decimal(4,2),
+    dht11_umidade decimal(4,2),
+    dataHora timestamp default current_timestamp
 );
 
+insert into registro (lm35_temperatura, dht11_umidade) values
+(20.10, 20.10);
+
+
+drop table registro;
 	select * from registro;
     truncate registro;
 
