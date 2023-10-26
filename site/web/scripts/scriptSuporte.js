ca7792cf880c64c5aa4936b5de2e247338c450b1
buenofonsecag <gustavo.fonseca@sptech.school>
2023-10-26 19:31:54 -0300

    validação de email no suporte e alteração para fechar depois que enviar a msg

--- site/web/scripts/scriptSuporte.js
+++ site/web/scripts/scriptSuporte.js
@@ -1,39 +1,46 @@
 
 function OpenSuporte() {
 
-    var  openSup = document.getElementById("containerSuporte");
+    var openSup = document.getElementById("containerSuporte");
 
-        openSup.style.display = `block`;
+    openSup.style.display = `block`;
 
     var closeImg = document.getElementById("idimgOpen")
 
-        closeImg.style.display = `none`
+    closeImg.style.display = `none`
 
-  
+
 }
 
 function ClosedSuporte() {
-    
-    var  openSup = document.getElementById("containerSuporte");
 
-        openSup.style.display = `none`;
+    var openSup = document.getElementById("containerSuporte");
+
+    openSup.style.display = `none`;
 
     var closeImg = document.getElementById("idimgOpen")
 
-        closeImg.style.display = `block`
+    closeImg.style.display = `block`
 }
 
-function enviarMsgSuporte(){
+function enviarMsgSuporte() {
+    var openSup = document.getElementById("containerSuporte");
+    var closeImg = document.getElementById("idimgOpen")
 
     var Input1Suporte = input_problema.value;
     var Input2Suporte = DescricaoDoPloblema.value;
     var Input3Suporte = EmailParaContato.value
 
-    if ( Input1Suporte == "" || Input2Suporte == "" || Input3Suporte == "") {
-        alert("!!! Preencha todos os campos !!!")
-    } else {
-        alert("Mensagem Enviada")
+    if (Input1Suporte == "" || Input2Suporte == "" || Input3Suporte == "") {
+        alert(" Preencha todos os campos !")
+
+    }else if (Input3Suporte.indexOf("@") == -1) {
+            alert("Insira um e-mail válido")
+        } else {
+            alert("Mensagem Enviada")
+            openSup.style.display = `none`       
+            closeImg.style.display = `block`
+        }
     }
 
-    
-}
+
